38b08767a4f5701afcb2bd2ca3fa7b67ba369624
Rohaan <rohaan.wasim@gmail.com>
2022-06-12 23:17:49 +0000

    Add Input Fields & Log New Fields

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,40 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // Get the value entered in the form.
+    //String textValue = request.getParameter("text-input");
+    String name = request.getParameter("user-name");
+    String email = request.getParameter("user-email");
+    String message = request.getParameter("user-message");
+    String funFact = request.getParameter("user-fun-fact");
+
+    // Print the value so you can see it in the server logs.
+    //System.out.println("You submitted: " + textValue);
+
+    // Print out what the user entered
+    //System.out.println("Name: " + name + "\nEmail: " + email + "\nMessage: " + message + (funFact.length() > 0 ? ("\nFun Fact" + funFact) : ""));
+    System.out.println("{Name: " + name + "; Email: " + email + "; Message: " + message + (funFact.length() > 0 ? ("; Fun Fact: " + funFact) : "") + "}");
+    /*System.out.println("Email: " + email);
+    System.out.println("Message: " + message);
+    if(funFact.length() > 0)
+        System.out.println("Fun Fact: " + funFact);
+    */
+
+    // Write the value to the response so the user can see it.
+    //response.getWriter().println("You submitted: " + textValue);
+
+    //Redirect To Home Page
+    response.sendRedirect("http://rwasim-sps-summer22.appspot.com/");
+  }
+}
